90c25b12ce81e0f353b32e017882b60f7255a871
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-07-16 23:02:45 +1000

    1.[UPDATE]:using if constexpr(c++17) output ASCII code when datatypes are same as uint8, char, int8 2.[UPDATE]:modify character width control logic

--- include/hexdump/hexdump.hpp
+++ include/hexdump/hexdump.hpp
@@ -15,8 +15,9 @@ namespace hexdump
 }
 
 template<typename Container>
-void hexdump::hexdump(const Container& view, const std::size_t width)
-{
+void hexdump::hexdump(const Container& view, const std::size_t width){
+          using value_type = typename Container::value_type;
+
           /*using ranges-v3 library! 8 characters are as a block*/
           std::size_t address{};
           for (auto chunks : view | ranges::views::chunk(width)) {
@@ -25,29 +26,37 @@ void hexdump::hexdump(const Container& view, const std::size_t width)
 
                     /*display hex code*/
                     for (auto c : chunks/*size = width*/) {
-                              unsigned char cast = static_cast<unsigned char>(c);
-                              std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(cast) << ' ';
+                              auto cast = std::make_unsigned_t<value_type>(c);
+                              std::cout << std::hex << std::setfill('0') << std::setw(2 * sizeof(value_type)) 
+                                             << static_cast<unsigned long long>(cast) << ' ';
                     }
 
-                    /*add redundant space(' ')*/
-                    for (std::size_t i = 0; i < (width - chunks.size()) * std::strlen("   "); ++i) {
-                              std::cout << ' ';
-                    }
+                    /*only uin8_t, int8_t, char datatypes could have access to ASCII code*/
+                    if constexpr (std::is_same_v<value_type, std::uint8_t> ||
+                              std::is_same_v<value_type, std::int8_t> ||
+                              std::is_same_v < value_type, char>) {
 
-                    /*start to display ASCII code*/
-                    std::cout << "     | ";
-                    for (auto c : chunks/*size = width*/) {
-                              /*should not be control character*/
-                              unsigned char cast = static_cast<unsigned char>(c);
-                              std::cout << (!std::isprint(cast) ? '.' : static_cast<char>(cast));
-                    }
+                              /*add redundant space(' ')*/
+                              for (std::size_t i = 0; i < (width - chunks.size()) * std::strlen("   "); ++i) {
+                                        std::cout << ' ';
+                              }
 
-                    /*add redundant space(' ')*/
-                    for (std::size_t i = 0; i < (width - chunks.size()); ++i) {
-                              std::cout << ' ';
-                    }
+                              /*start to display ASCII code*/
+                              std::cout << "     | ";
+                              for (auto c : chunks/*size = width*/) {
+                                        /*should not be control character*/
+                                        unsigned char cast = static_cast<unsigned char>(c);
+                                        std::cout << (!std::isprint(cast) ? '.' : static_cast<char>(cast));
+                              }
 
-                    std::cout << " |\n";
+                              /*add redundant space(' ')*/
+                              for (std::size_t i = 0; i < (width - chunks.size()); ++i) {
+                                        std::cout << ' ';
+                              }
+                              std::cout << " |";
+                    }
+                    std::cout << '\n';
+
                     address += chunks.size();
           }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 
 int main() 
 {
-          std::string s = "hello, world!\n";
+          //std::string s = "hello, world!\n";
+          //std::random_device rd;
+          //std::mt19937 mt(rd());  //init a random 
+          //std::uniform_int_distribution<std::size_t> uid(0, 0xff);
+          //for (int i = 0; i < 1000; ++i) {
+          //          s.push_back(uid(mt));
+          //}
+          //hexdump::hexdump(s);
+
+          std::vector<uint16_t> s;
           std::random_device rd;
-          std::mt19937 mt(rd());  //init a random 
-          std::uniform_int_distribution<std::size_t> uid(0, 0xff);
+          std::mt19937 mt(rd());
+          std::uniform_int_distribution<std::int16_t> uid(-0x8000, 0x7fff);
           for (int i = 0; i < 1000; ++i) {
                     s.push_back(uid(mt));
           }
